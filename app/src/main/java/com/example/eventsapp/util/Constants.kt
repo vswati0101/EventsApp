98d393dbf2cc01784f15c3f0cb12b53558f8cbdb
Swati Verma <swati.verma@reddoorz.com>
2024-04-19 11:04:07 +0530

    added username

--- app/src/main/java/com/example/eventsapp/util/Constants.kt
+++ app/src/main/java/com/example/eventsapp/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.eventsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "KZqcF3UfhkQACI201PVG1J7oB40xWLqo"
+        const val API_KEY = "YOUR_API_KEY"
         const val BASE_URL = "https://app.ticketmaster.com/"
         const val QUERY_PAGE_SIZE = 20
     }
